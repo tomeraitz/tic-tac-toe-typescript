947cfbf7863dae3f866fc85374ca04a447c079f2
tomeraitz <tomeraitz1@gmail.com>
2019-03-20 17:23:07 +0200

    uploaded to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,6 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/'
+    path: path.resolve(__dirname, 'dist')
   }
 }
